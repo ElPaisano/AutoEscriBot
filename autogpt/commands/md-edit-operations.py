8cdc338bcfcb97164b9b12c2bba22db5d14b9e1a
ElPaisano <113373882+ElPaisano@users.noreply.github.com>
2023-05-03 23:07:58 -0600

    Init md operations

--- autogpt/commands/md-edit-operations.py
+++ autogpt/commands/md-edit-operations.py
@@ -1,70 +1,51 @@
-#!/usr/bin/env python3
+"""Markdown edit operations for AutoGPT"""
 
+from __future__ import annotations
+
+from spellchecker import SpellChecker
 import subprocess
 import re
 import sys
+import mistune
 
-DOCLINK = ""
-bold = "\033[1m"
-normal = "\033[0m"
-regular_bar = "-" * 52
-bold_bar = "=" * 52
-error_bar = "!**************************************************!"
 
-file_list = subprocess.check_output(["git", "diff", "--diff-filter=d", "--cached", "--name-only"]).decode().splitlines()
-md_file_list = list(filter(lambda x: x.endswith(".md"), file_list))
-
-if md_file_list:
-    errors = 0
-    print("\n", "The following files were changed:")
-    for file in md_file_list:
-        print(f" - {file}")
-    print(bold_bar)
-
-    print("\n", regular_bar)
-    print(f"{bold}Spell check{normal}")
-    print(regular_bar)
-    spell_passed = subprocess.call(["npx", "mdspell", "-r", "-a", "-n", "--en-us"] + md_file_list)
+def extract_text_from_markdown(markdown):
+    # Remove code blocks
+    code_block_pattern = r"```.*?```"
+    markdown = re.sub(code_block_pattern, "", markdown, flags=re.DOTALL)
     
-    print("\n", regular_bar)
-    print(f"{bold}Link check{normal}")
-    print(regular_bar)
-    links_passed = subprocess.call(["npx", "markdown-link-check", "-q", "-p"] + md_file_list)
+    # Remove inline code
+    inline_code_pattern = r"`.*?`"
+    markdown = re.sub(inline_code_pattern, "", markdown)
 
-    print("\n", regular_bar)
-    print(f"{bold}Formatting check{normal}")
-    print(regular_bar)
-    format_passed = subprocess.call(["npx", "markdownlint-cli2"] + md_file_list)
+    # Remove URLs
+    url_pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    markdown = re.sub(url_pattern, "", markdown)
 
-    error_descr = ""
+    # Remove Markdown formatting
+    formatting_pattern = r"#|\*|\[|\]|\(|\)|\||>|`|-|_"
+    text = re.sub(formatting_pattern, "", markdown)
+    return text
 
-    if links_passed != 0:
-        error_descr += "\n- Broken links."
-        errors = 1
-    if spell_passed != 0:
-        error_descr += "\n- Spelling errors."
-        errors = 1
-    if format_passed != 0:
-        error_descr += "\n- Markdown formatting errors."
-        errors = 1
+def get_md_files_list() -> List:
+    file_list = subprocess.check_output(["git", "diff", "--diff-filter=d", "--cached", "--name-only"]).decode().splitlines()
+    return list(filter(lambda x: x.endswith(".md"), file_list))
 
-    if errors == 1:
-        print("\n", error_bar)
-        print(f"{bold}ERRORS FOUND{normal}")
-        print("There are some problems with your commit:")
-        print(error_descr)
-        print("\n", "For details on how to fix these errors, see")
-        print(f"{DOCLINK}.")
-        print(error_bar, "\n")
-        sys.exit(1)
-    else:
-        print("\n", regular_bar)
-        print(f"{bold}No errors were found!{normal}")
-        sys.exit(0)
+@command("get_mispelled_words", "Check mispelled words", '"filename": "<filename>"')
+def get_mispelled_words(filename):
+    text = extract_text_from_markdown(filename)
+    spell = SpellChecker()
+    words = text.split()
+    return spell.unknown(words)
 
-else:
-    print("\n", regular_bar)
-    print("No markdown files were changed in this commit.")
-    print("Skipping checks...")
-    print(regular_bar, "\n")
-    sys.exit(0)
+def check_markdown_formatting(markdown_content):
+    return mistune.markdown(markdown_content)
+
+def broken_links_exist():
+    return subprocess.call(["markdown-link-check", "-q", "-p"] + md_file_list)
+
+def format_errors_exist():
+    return subprocess.call(["npx", "markdownlint-cli2"] + md_file_list)
+
+def md_files_exist():
+    return True if md_file_list else False
